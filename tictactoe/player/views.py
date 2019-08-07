3f52d43a00915bb4e949c7e4d924866587667c34
mariah <mariah.kakande@gmail.com>
2019-08-07 10:36:51 +0300

    ft(login): add login page - add login feature -add logout feature
    [Delivers #167724866]

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from gameplay.models import Game
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def home(request):
     my_games = Game.objects.games_for_user(request.user)
     active_games = my_games.active()
